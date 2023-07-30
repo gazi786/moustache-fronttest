cde28bf0eafdb9d276b3df72fd0720be3d58eb3a
Alshafaraz Gazi <alshafaraz.gazi@gmail.com>
2023-07-31 09:51:14 +1200

    Button Update

--- src/components/Utils/CustomRadioButton.jsx
+++ src/components/Utils/CustomRadioButton.jsx
@@ -4,15 +4,18 @@ import { Radio } from "@mui/material";
 import "./CustomRadioButton.css";
 
 const CustomRadioButton = ({ value, label, selectedValue, onChange }) => {
+  const [checked, setChecked] = React.useState(false);
+
   return (
     <label className="custom-radio">
+      <span className="custom-radio-label"> {label}</span>
       <Radio
         value={value}
-        checked={selectedValue === value}
-        onChange={onChange}
+        checked={checked}
+        onChange={(event) => setChecked(event.target.checked)}
         className="custom-radio-input"
       />
-      <span className="custom-radio-label">{label}</span>
+      <span className="checkmark" />
     </label>
   );
 };
